839bd0d2574e316a52e1541e353ea949b08f9c9e
wwylele <wwylele@gmail.com>
2016-11-16 19:05:34 +0200

    or without sdl?

--- src/tests/sample.cpp
+++ src/tests/sample.cpp
@@ -2,15 +2,15 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#include <SDL.h>
+// #include <SDL.h>
 #include <catch.hpp>
-// #include <glad/glad.h>
+#include <glad/glad.h>
 #include "common/file_util.h"
 #include "core/core_timing.h"
 
 TEST_CASE("Dummy", "[test]") {
-    // gladLoadGLLoader([](const char* name) -> void* { return 0; });
-    SDL_GL_GetProcAddress("");
+    gladLoadGLLoader([](const char* name) -> void* { return 0; });
+    // SDL_GL_GetProcAddress("");
     REQUIRE(1 + 1 == 2);
     REQUIRE(CoreTiming::GetClockFrequencyMHz() != 0);
 }
