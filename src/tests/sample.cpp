07c28ae6758e491a1e40b13122bacaf24aa57931
wwylele <wwylele@gmail.com>
2016-11-16 15:26:30 +0200

    add a test

---
+++ src/tests/sample.cpp
@@ -0,0 +1,13 @@
+// Copyright 2016 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#include <catch.hpp>
+
+#include "common/file_util.h"
+#include "core/core_timing.h"
+
+TEST_CASE("Dummy", "[test]") {
+    REQUIRE(1 + 1 == 2);
+    REQUIRE(CoreTiming::GetClockFrequencyMHz() != 0);
+}
