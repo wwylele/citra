89d139ce89201b78196a7a00359458ca56d110d6
wwylele <wwylele@gmail.com>
2016-11-16 16:34:36 +0200

    explicitly refer to glad and SDL

--- src/tests/sample.cpp
+++ src/tests/sample.cpp
@@ -2,12 +2,14 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <SDL.h>
 #include <catch.hpp>
-
+#include <glad/glad.h>
 #include "common/file_util.h"
 #include "core/core_timing.h"
 
 TEST_CASE("Dummy", "[test]") {
+    gladLoadGLLoader(static_cast<GLADloadproc>(SDL_GL_GetProcAddress));
     REQUIRE(1 + 1 == 2);
     REQUIRE(CoreTiming::GetClockFrequencyMHz() != 0);
 }
