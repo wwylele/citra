88f1e97ba3d02e090111e3df2a479dea03888cea
wwylele <wwylele@gmail.com>
2016-11-16 20:28:38 +0200

    remove sdl completely

--- src/tests/sample.cpp
+++ src/tests/sample.cpp
@@ -2,7 +2,6 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-// #include <SDL.h>
 #include <catch.hpp>
 #include <glad/glad.h>
 #include "common/file_util.h"
@@ -10,7 +9,6 @@
 
 TEST_CASE("Dummy", "[test]") {
     gladLoadGLLoader([](const char* name) -> void* { return 0; });
-    // SDL_GL_GetProcAddress("");
     REQUIRE(1 + 1 == 2);
     REQUIRE(CoreTiming::GetClockFrequencyMHz() != 0);
 }
